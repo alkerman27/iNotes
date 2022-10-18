e2cea772442445271dc75a34de000274ee1629aa
Mark AC <m4rkd0tc0de@gmail.com>
2022-10-19 04:28:20 +0800

    whitelisting website to be allowed in the server using CORS

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://www.alkermantechshop.com',
+  'https://alkermantechshop.com',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3500;
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
+
+const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
